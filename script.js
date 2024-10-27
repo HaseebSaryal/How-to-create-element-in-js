655530be8052bc3730d08e150e1e5157022eb088
HaseebSaryal <haseebsaryal@gmail.com>
2024-10-27 22:30:54 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// How to create element in js
+
+// const div = document.querySelector(".parent");
+// div.children[0].style.color = "blue"
+// console.log(div.children)
+
+const element = document.querySelector("h1")
+// element.className = "main"
+// element.id = Math.floor(Math.random() * 10 + 2)
+// element.style.innerHTML = "Hellow Div"
+// element.setAttribute("title", "created")
+// element.style.color = "yellow"
+
+const dox = document.createTextNode("Chai or Haseeb")
+element.appendChild(dox)
+document.body.appendchild(h1)
+
+
